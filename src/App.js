e5751dbd12a0cdda1cc02bf451a09bbe2a9a3220
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-09-01 15:36:09 -0600

    backgrounds with parallex effect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
 import logo from './images/logo.svg';
 import bg from './images/mountains.svg';
+import sky from './images/pexelsNightSky.jpg';
 import './App.css';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import { Typography } from '@mui/material';
+import Bio from './components/Bio';
 
 function App() {
   return (
     <div className="App">
-      <Parallax pages={4}>
-        <ParallaxLayer offset={0} 
-          speed={1}
-          style={{backgroundImage:`url(${bg})`}}
+      <Parallax pages={5}>
+        <ParallaxLayer 
+          offset={0} 
+          factor={2}
+          speed={1.3}
+          style={{
+            backgroundImage:`url(${bg})`, 
+            backgroundSize: 'cover'
+          }}
           >
-          <Typography variant="h2" >Welcome to my Website!</Typography>
         </ParallaxLayer>
-        
-        <ParallaxLayer offset={1} speed={0.5}>
-          <Typography variant="h2" >This is the second page!</Typography>
+        <ParallaxLayer 
+          offset={0.4}
+          speed={0.5}
+        >
+          <Typography variant="h2" color="white">Welcome!</Typography>
+        </ParallaxLayer>
+
+        <ParallaxLayer 
+          offset={1} 
+          speed={0.5} 
+          factor={3}
+          style={{
+            backgroundImage:`url(${sky})`,
+            backgroundSize: 'cover',
+          }}
+          >
+        </ParallaxLayer>
+        <ParallaxLayer 
+          offset={1.5}
+          speed={0.3}
+          style={{
+            color:'white'
+          }}
+        >
+          <Typography variant="h2" >This is what I've been working on lately!</Typography>
         </ParallaxLayer>
       </Parallax>
     </div>
