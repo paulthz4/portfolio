24b1e6b2d827db99805889e0b661225a8b4bffe2
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-09-02 15:09:05 -0600

    set background

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './images/logo.svg';
 import bg from './images/mountains.svg';
-import sky from './images/pexelsNightSky.jpg';
+import trees from './images/trees.svg';
 import './App.css';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import { Typography } from '@mui/material';
@@ -8,12 +8,12 @@ import Bio from './components/Bio';
 
 function App() {
   return (
-    <div className="App">
-      <Parallax pages={5}>
+    <div className="App" style={{ width: '100%', height: '100%', background: '#1F315B' }}>
+      <Parallax pages={3}>
         <ParallaxLayer 
           offset={0} 
           factor={2}
-          speed={1.3}
+          speed={1}
           style={{
             backgroundImage:`url(${bg})`, 
             backgroundSize: 'cover'
@@ -21,30 +21,40 @@ function App() {
           >
         </ParallaxLayer>
         <ParallaxLayer 
-          offset={0.4}
-          speed={0.5}
+          offset={1}
+          factor={1}
+          speed={1}
+        >
+         <div style={{backgroundColor:'#91D5A1', width: '100%', height:'100%'}}></div>
+        </ParallaxLayer>
+        <ParallaxLayer 
+          offset={1}
+          factor={1}
+          speed={1}
+          style={{
+            backgroundImage: `url(${trees})`,
+            backigroundSize:'cover',
+          }}
+        />
+        <ParallaxLayer 
+          offset={1.9} 
+          factor={2.1}
+          speed={1}
+          >
+          <div style={{backgroundColor:'#669D73', width: '100%', height:'100%'}}></div>
+        </ParallaxLayer>
+        <ParallaxLayer 
+          offset={0.5}
+          speed={0.2}
         >
           <Typography variant="h2" color="white">Welcome!</Typography>
         </ParallaxLayer>
 
         <ParallaxLayer 
-          offset={1} 
-          speed={0.5} 
-          factor={3}
-          style={{
-            backgroundImage:`url(${sky})`,
-            backgroundSize: 'cover',
-          }}
-          >
-        </ParallaxLayer>
-        <ParallaxLayer 
-          offset={1.5}
-          speed={0.3}
-          style={{
-            color:'white'
-          }}
+          offset={1.7}
+          speed={0.2}
         >
-          <Typography variant="h2" >This is what I've been working on lately!</Typography>
+          <Typography variant="h2" color="white" >This is what I've been working on lately!</Typography>
         </ParallaxLayer>
       </Parallax>
     </div>
