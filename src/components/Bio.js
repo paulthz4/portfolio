2c6d7e9c76b1fb792e7255b6cdc7d6c0abd8cca3
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-09-08 20:54:56 -0600

    info text to bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -13,15 +13,18 @@ export default function Bio(){
         speed={0.4}
       >
         <Box className="container" sx={{margin:'auto',  width: '40%' }}>
-          <Card sx={{width: '95%' }}>
+          <Card elevation={4} sx={{width: '95%' }}>
             <CardHeader 
-              avatar={<Avatar alt="Paul Zapote" src={pic} sx={{width:200, height:200, maxWidth:'100%', mx:4}} />}
+              avatar={<Avatar alt="Paul Zapote" src={pic} sx={{width:200, height:200, maxWidth:'100%', marginLeft:5}} />}
               title={<Typography fontSize='medium'>Hi there! My name is Paul Zapote</Typography>}
               subheader={<Typography variant='p' fontSize='small' sx={{opacity:0.5}}><RoomOutlinedIcon fontSize='small'/> Calgary, Alberta, Canada</Typography>}
               />
-              <CardContent>
-                <Typography variant='body2'>
-                
+              <CardContent sx={{p:5}}>
+                <Typography variant='body2' sx={{textAlign:'justify'}}>
+                  I graduated from the University of British Columbia in 2021 with a B.Sc. degree in Computer Science
+                  and a minor in Economics. I spend the majority of my free time working on some passions projects that inlove 
+                  building web apps that make things more efficient or make tasks a little easier as you can see some of the projects below.
+                  I spend the rest of time doing other hobbies like hiking, camping, traveling, and playing sports when I'm not coding!
                 </Typography>
               </CardContent>
           </Card>
